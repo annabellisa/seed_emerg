c13ea0c7e60cac120e85bc8de31273d009cf8aba
cgask <caitlingask@icloud.com>
2024-02-22 12:09:28 +1000

    script to extract pfull data

--- 03_script/01_process_data.R
+++ 03_script/01_process_data.R
@@ -1,4 +1,32 @@
 # Analysis script
 
 # Author Caitlin Gaskell
+#inport plant full
 
+pfull<-read.table("01_data/plant_full.txt",header=T)
+head(pfull,4);dim(pfull)
+
+
+#import site data
+
+sdata<-read.csv("01_data/site_data.csv",header=T)
+head(sdata,4);dim(sdata)
+
+sdata$quadratID<-paste(sdata$transect,sdata$quadrat,sep=".")
+
+#extract from pfull unique quadrats
+
+soilq<-unique(sdata$quadratID)
+soilq
+
+
+#check that soilqs exist in pfull
+
+missingq<-sdata[which(!soilq %in% unique(pfull$quadratID)),]$quadratID
+
+soilq<-soilq[which(!soilq==missingq)]
+
+
+psoil<-pfull[which(pfull$quadratID %in% soilq),]
+head(psoil);dim(psoil)
+rownames(psoil)<-1:nrow(psoil)
